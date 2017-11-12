7216ebd247c0b92923aaf6e795cb7e8e0531e49e
osmarluz <osmar.luz@gmail.com>
2017-11-12 20:21:00 -0200

    Configure user, campaign and member models

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,17 @@
 class Campaign < ApplicationRecord
+  enum status: [:pending, :finished]
+  
   belongs_to :user
+  has_many :members, dependent: :destroy
+  before_create :set_status
+  before_create :set_member
+  validates :title, :description, :user, :status, presence: true
+
+  def set_status
+    self.status = :pending
+  end
+
+  def set_member
+    self.members << Member.create(name: self.user.name, email: self.user.email)
+  end
 end
